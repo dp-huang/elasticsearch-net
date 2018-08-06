41d06da3627ad8c3769dc496dcfc6d61a756ec86
Mpdreamz <mpdreamz@gmail.com>
2018-08-06 20:06:47 +0200

    Tests.Core namespace adjust

--- src/Tests/Tests/Framework/EndpointTests/ApiIntegrationTestBase.cs
+++ src/Tests/Tests/Framework/EndpointTests/ApiIntegrationTestBase.cs
@@ -6,6 +6,9 @@ using Elastic.Managed.Ephemeral;
 using Elastic.Xunit.XunitPlumbing;
 using FluentAssertions;
 using Nest;
+using Tests.Core.Client;
+using Tests.Core.Extensions;
+using Tests.Core.ManagedElasticsearch.Clusters;
 using Tests.Framework.Integration;
 using Tests.Framework.ManagedElasticsearch.Clusters;
 
